5d3b25ff375412a77dcc23910f63700509050ec5
Hadhad / HanoHano <katai5plate.sub@gmail.com>
2025-01-17 21:34:55 +0900

    Update install-work-2025.ps1

--- install-work-2025.ps1
+++ install-work-2025.ps1
@@ -1,2 +1,3 @@
+(iwr "https://raw.githubusercontent.com/katai5plate/startup-my-PC_install/refs/heads/master/scoop-work-2025.txt").Content > scoop-work.txt
 scoop install ./scoop-work.txt
 nvm install 16; nvm use 16

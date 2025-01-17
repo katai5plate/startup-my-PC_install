f4e7a3ee34fbc9963b1fb7f3ce5d3b26fe223eb9
Hadhad / HanoHano <katai5plate.sub@gmail.com>
2025-01-17 21:35:08 +0900

    Update install-make-2025.ps1

--- install-make-2025.ps1
+++ install-make-2025.ps1
@@ -1,3 +1,5 @@
+(iwr "https://raw.githubusercontent.com/katai5plate/startup-my-PC_install/refs/heads/master/scoop-work-2025.txt").Content > scoop-work.txt
+(iwr "https://raw.githubusercontent.com/katai5plate/startup-my-PC_install/refs/heads/master/scoop-make-2025.txt").Content > scoop-make.txt
 scoop install ./scoop-work.txt
 scoop install ./scoop-make.txt
 nvm install 16; nvm use 16
